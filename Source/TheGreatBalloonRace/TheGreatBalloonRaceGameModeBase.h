c1f10b8108781c395d12538d0ec7ee7873d9f020
MMC-Scholars <latrunkster@gmail.com>
2019-01-31 15:50:23 -0500

    Initial Commit

---
+++ Source/TheGreatBalloonRace/TheGreatBalloonRaceGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheGreatBalloonRaceGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEGREATBALLOONRACE_API ATheGreatBalloonRaceGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};

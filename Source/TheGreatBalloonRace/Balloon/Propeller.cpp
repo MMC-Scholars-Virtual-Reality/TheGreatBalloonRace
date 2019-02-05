83fc618f65a53dfaae210b0d54de2ec144b47c46
MMC-Scholars <latrunkster@gmail.com>
2019-02-05 15:17:16 -0500

    Update Propeller.cpp

---
+++ Source/TheGreatBalloonRace/Balloon/Propeller.cpp
@@ -0,0 +1,19 @@
+#include "Propeller.h"
+
+
+void addEnergy(joules jEnergy)
+{
+  m_currentEnergy = m_currentEnergy + jEnergy;
+}
+
+newtons getPropulsionStrength()
+{
+
+}
+
+
+void think()
+{
+
+}
+

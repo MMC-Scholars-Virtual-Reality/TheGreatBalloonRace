f678d5bd32bb763dc6edbb5b68fc7eaf95a5c9ae
MMC-Scholars <latrunkster@gmail.com>
2019-02-07 15:17:27 -0500

    Update Propeller.cpp

--- Source/TheGreatBalloonRace/Balloon/Propeller.cpp
+++ Source/TheGreatBalloonRace/Balloon/Propeller.cpp
@@ -2,19 +2,23 @@
 #include "Windows.h"
 
 
-void addEnergy(joules jEnergy)
+void Propeller::addEnergy(joules jEnergy)
 {
-  joules m_currentEnergy = m_currentEnergy + jEnergy;
+    m_currentEnergy += jEnergy;
 }
-
-newtons getPropulsionStrength()
+//get equation of prop strength based on rotational velocity, forces and parameters we can actually control
+newtons Propeller::getPropulsionStrength()
 {
-
+    double density; //air density
+    double area; //area of the propeller, modified by the number of blades
+    double exitV; //the velocity of the air leaving the propeller
+    double aircraftV; //the velocity of the aircraft
+    m_propStrength = .50 * density * area *((Math.pow(exitV, 2) - Math.pow(aircraftV, 2));
+    return m_propStrength;
 }
-
-
-void think()
+		//calculate rotational velocity based on energy, time, mass, and drag
+void Propeller::think()
 {
-
+    
 }
 

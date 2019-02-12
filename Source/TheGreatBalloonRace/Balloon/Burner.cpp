f6818a58a1788e4dfdff8725ce0c472df3bca7f5
MMC-Scholars <latrunkster@gmail.com>
2019-02-12 16:59:23 -0500

    Further outlined burner class - Michael

--- Source/TheGreatBalloonRace/Balloon/Burner.cpp
+++ Source/TheGreatBalloonRace/Balloon/Burner.cpp
@@ -1,9 +1,11 @@
 #include "Burner.h"
 #include "VRBase/System/Globals.h"
 
-Burner::AirVolume::AirVolume(const float _AirVolume, const float _BalloonRadius) {
-	m_volume = _AirVolume;
+Burner::AirVolume::AirVolume(const float _BalloonRadius) {
 	m_radius = _BalloonRadius;
+
+	//Use half-sphere volume equation
+	m_volume = 2.f * 3.f * PI * powf(m_radius, 3);
 }
 
 void Burner::AirVolume::addEnergy(const joules heat) {
@@ -11,9 +13,5 @@ void Burner::AirVolume::addEnergy(const joules heat) {
 }
 
 void Burner::AirVolume::transferHeatTo(meters area, kelvin outsideTemp, AirVolume* pOtherVolume) {
-	kelvin heat = 
-}
-
-void Burner::stopApplyingHeat() {
-
+	//kelvin heat = 
 }
--- Source/TheGreatBalloonRace/Balloon/Burner.h
+++ Source/TheGreatBalloonRace/Balloon/Burner.h
@@ -1,11 +1,14 @@
 #pragma once
 #include "predefs.h"
+
+class FuelTank;
+
 /**
  * Burner models the main fuel burner which sends hot gas into the balloon, and also models
  * the state of the air inside the balloon
  */
 class Burner {
-
+private:
 	class AirVolume {
 
 	private:
@@ -15,14 +18,33 @@ class Burner {
 		meters m_radius;
 
 	public:
-		AirVolume(const float _AirVolume, const float _BalloonRadius);
+		AirVolume(const float _BalloonRadius);
 
 		void addEnergy(const joules heat);
 
 		void transferHeatTo(meters area, kelvin outsideTemp, AirVolume* pOtherVolume = nullptr);
 
+		//Based on temperature and other parameters, updates energy
+		void updateParametersFromTemperature();
+
+		//Based on energy and other parameters, updates temperature
+		void updateParametersFromEnergy();
+
 	} m_bottomState, m_topState;
 
-	void stopApplyingHeat();
+	//Fuel tank to consume from
+	FuelTank* m_pFuelTank;
 
+	lerp m_throttel; //Is the burner on or off, on a range from 0 to 1
+
+public:
+	//Called on each frame
+	//Based on the throttel level, consumes an amount of fuel from the fueltank
+	//and adds energy to m_bottomState
+	void Think();
+
+	//Based on atmospheric conditions and m_topState, calculates a buoyant force in newtons
+	newtons GetBuoyantForce();
+
+	void SetThrottelLevel(lerp _throttel);
 };

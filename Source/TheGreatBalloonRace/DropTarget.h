6cbb16a74441759f6fedf095ca9cc72e8f9f607b
MMC-Scholars <latrunkster@gmail.com>
2019-02-13 18:29:04 -0500

    Created ADropTarget -Noah

---
+++ Source/TheGreatBalloonRace/DropTarget.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VRBase/ABaseEntity/ABaseEntity.h"
+#include "DropTarget.generated.h"
+#include "Components/StaticMeshComponent.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEGREATBALLOONRACE_API ADropTarget : public ABaseEntity
+{
+	GENERATED_BODY()
+	
+public:
+
+	ADropTarget();
+
+	
+
+		UStaticMeshComponent*	m_pPickupMeshComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pickup Static Mesh")
+		UStaticMesh*			staticMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Pickup Materials")
+		UMaterialInterface*		material0;
+	
+	
+};

775535a74a2630c4a6d7cbee9d88c413e6574687
MMC-Scholars <latrunkster@gmail.com>
2019-02-13 15:15:39 -0500

    continued researching propulsion strength, changed new member variable names to match the syntax of others - Ryan

--- Source/TheGreatBalloonRace/Balloon/Propeller.cpp
+++ Source/TheGreatBalloonRace/Balloon/Propeller.cpp
@@ -3,21 +3,33 @@
 
 void Propeller::addEnergy(joules jEnergy)
 {
-    m_currentEnergy += jEnergy;
+	m_jCurrentEnergy += jEnergy;
 }
-//get equation of prop strength based on rotational velocity, forces and parameters we can actually control
+//get equation of prop strength based on rotational velocity, 
+//forces and parameters we can actually control
 newtons Propeller::getPropulsionStrength()
 {
-    double density = 0; //air density
-    double area = 0; //area of the propeller, modified by the number of blades
-    double exitV = 0; //the velocity of the air leaving the propeller
-    double aircraftV = 0; //the velocity of the aircraft
-    m_propStrength = .50 * density * area * (sqr(exitV) - sqr(aircraftV));
-    return m_propStrength;
+	double density = 1.225; //air density
+	double area = 0; //area of the propeller disk
+	double exitV = 0; //the velocity of the air leaving the propeller
+	double aircraftV = 0; //the velocity of the aircraft
+	//torque = rotational velocity^2, does not account for the other forces(blade pitch, etc)
+	//F = 4.392399x10^-8 * RPM diameter(in inches)^(3.5)/(sqrt(pitch(in inches)) * (4.23333x10^-4) * RPM * pitch(in inches) - V0(propeller forward airspeed)
+	//static thrust if V0 = 0
+	m_nPropStrength = .50 * density * area * (sqr(exitV) - sqr(aircraftV));
+	return m_nPropStrength;
 }
-		//calculate rotational velocity based on energy, time, mass, and drag
+//calculate rotational velocity based on energy, time, mass, and drag
 void Propeller::think()
 {
-    
+	//Keep if numBlades is even
+	m_nInertia = m_iNumBlades * (1 / 12.0) * m_dMass * sqr(2 * m_flBladeRadius);
+	//USE IF NUMBLADES IS ODD - inertia = 1/3 * m_mass * sqr(r);
+	m_dRotationalVelo = (2 * m_jCurrentEnergy) / m_nInertia;
+	/*
+	v = tangential speed = rotational speed * r, r = radial distance
+	Rotational Kinetic energy = .5 * rotational Inertia * rotSpeed
+	angular velocity = rotational velocity = v/r
+	drag = .5 * drag coefficient * density * area * velocity^2
+	*/
 }
-
--- Source/TheGreatBalloonRace/Balloon/Propeller.h
+++ Source/TheGreatBalloonRace/Balloon/Propeller.h
@@ -4,39 +4,41 @@
 #include "VRBase/System/predefs.h"
 
 class Propeller {
-/*
- * Private member variables
- */
+	/*
+	* Private member variables
+	*/
 private:
-	
+
 	uint8		m_iNumBlades; //number of blades on the propeller
 	vec			m_flBladeRadius; //the radius of the blade on the propeller
-	newtons		m_propStrength; //the strength of the propulsion/thrust of the propeller
-	double 		m_area; //surface area of the propeller disk, looking at it from the front(circle)
-	double		m_rotationalVelo; //revolutions per second of the propeller
-	double 		m_mass; //mass of the propeller
-	
+	double		m_dBladePitch; //the pitch of the blade
+	newtons		m_nPropStrength; //the strength of the propulsion/thrust of the propeller
+	double 		m_dArea; //surface area of the propeller disk, looking at it from the front(circle)
+	double		m_dRotationalVelo; //revolutions per second of the propeller
+	double 		m_dMass; //mass of the propeller
+
 	//TODO - add member variables needed for physics-based model of propeller
-/*
- * Public members
- */
-public:
-	joules		m_currentEnergy = 0;
 	/*
-	 * Adds energy to be converted into rotational energy of the propeller.
-	 */
+	* Public members
+	*/
+public:
+	joules		m_jCurrentEnergy = 0; //the current energy being given to the propellers
+	newtons		m_nInertia = 0; //the rotational inertia of the propeller
+	/*
+	* Adds energy to be converted into rotational energy of the propeller.
+	*/
 	void addEnergy(joules jEnergy);
 
 	/**
-	 * Based on current rotational velocity, blade radius, and number of blades,
-	 * Returns a force in newtons to be applied to a ForceAccumulator, same as thrust???
-	 * 
-	 */
+	* Based on current rotational velocity, blade radius, and number of blades,
+	* Returns a force in newtons to be applied to a ForceAccumulator
+	*
+	*/
 	newtons getPropulsionStrength();
 
 	/**
-	 * Models the decay of energy over time
-	 */
+	* Models the decay of energy over time
+	*/
 	void think();
 
 };

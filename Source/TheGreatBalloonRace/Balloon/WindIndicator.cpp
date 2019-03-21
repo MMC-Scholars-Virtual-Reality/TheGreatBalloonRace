b571140f3974d20a501dbcebeafdeea72cd36fb1
MMC-Scholars <latrunkster@gmail.com>
2019-03-21 15:12:56 -0400

    Updated WindIndicator - Ryan

--- Source/TheGreatBalloonRace/Balloon/WindIndicator.cpp
+++ Source/TheGreatBalloonRace/Balloon/WindIndicator.cpp
@@ -2,13 +2,17 @@
 #include "WindField.h"
 #include "ForceAccumulator.h"
 #include "Components/ArrowComponent.h"
+ENGINE_ArrowComponent_generated_h;
 
 //WiNDICATOR
-void AWindIndicator::DefaultThink() {
-	meters m = 0;
-	FVector WindDirection = WindField::GetWindDirectionAtAltitude(m);
+AWindIndicator::AWindIndicator() {
+	//m_pWindArrow->CreateDefaultSubobject<UArrowComponent>("WindArroww");
+	//m_pWindArrow->SetVisibility(true);
 	
-
 }
-
-
+void AWindIndicator::DefaultThink() {
+	meters m = GetActorLocation().Z;
+	FVector WindDirection = WindField::GetWindDirectionAtAltitude(m);
+	m_pWindArrow->SetWorldRotation(WindDirection.Rotation());
+	
+}
--- Source/TheGreatBalloonRace/Balloon/WindIndicator.h
+++ Source/TheGreatBalloonRace/Balloon/WindIndicator.h
@@ -12,9 +12,14 @@ UCLASS()
 class THEGREATBALLOONRACE_API AWindIndicator : public ABaseEntity
 {
 	GENERATED_BODY()
-	
+	//public member functions
 public:
+	AWindIndicator();
 	virtual void DefaultThink() override;
+
+	//private member variables
+private:
+	class UArrowComponent* m_pWindArrow;
 
 	
 	

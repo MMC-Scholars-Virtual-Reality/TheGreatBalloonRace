56789e8c4d6542429e21a072b653687151a952a9
MMC-Scholars <latrunkster@gmail.com>
2019-03-27 15:16:25 -0400

    started working on RudderController -Ryan

--- Source/TheGreatBalloonRace/Balloon/RudderController.cpp
+++ Source/TheGreatBalloonRace/Balloon/RudderController.cpp
@@ -1,7 +1,12 @@
 #include "RudderController.h"
 
 //inherit from ABaseLinear
-void think() {
+ARudderController::ARudderController() {
+	m_rudderController= CreateDefaultSubobject<UStaticMeshComponent>("m_RudderController");
+	pController = NULL;
+}
+void think(ABaseController* pController) {
+	ARudderController rudder1;
+	rudder1.SetPositionFromController(pController);
 
 }
-
--- Source/TheGreatBalloonRace/Balloon/RudderController.h
+++ Source/TheGreatBalloonRace/Balloon/RudderController.h
@@ -2,19 +2,26 @@
 
 #include "CoreMinimal.h"
 #include "VRBase/ABaseEntity/ABaseEntity.h"
+#include "VRBase/AMoveLinear/AMoveLinear.h"
 #include "RudderController.generated.h"
+
 
 /**
  * 
  */
 UCLASS()
-class THEGREATBALLOONRACE_API ARudderController : public ABaseEntity
+class THEGREATBALLOONRACE_API ARudderController : public ABaseMoving
 {
 	GENERATED_BODY()
 //public member functions
 public:
-		void think();
-	
-	
+	ARudderController();
+	void think(ABaseController* pController);
+
+//public member variables
+public:
+	ABaseController* pController;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "ARudderController")
+		UStaticMeshComponent* m_rudderController;
 	
 };

66f36611c2fc2c71467500d860f9796cb3359630
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2019-09-27 14:55:12 -0400

    removed unnecessary project files - Sam

--- Source/TheGreatBalloonRace/TheGreatBalloonRace.Build.cs
+++ Source/TheGreatBalloonRace/TheGreatBalloonRace.Build.cs
@@ -1,26 +1,15 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+using System.IO;
 using UnrealBuildTool;
 
 public class TheGreatBalloonRace : ModuleRules
 {
-	public TheGreatBalloonRace(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+    public TheGreatBalloonRace(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Slate", "SlateCore" });
-
         PrivateDependencyModuleNames.AddRange(new string[] { "HeadMountedDisplay", "SteamVR" });
 
-        PublicIncludePaths.Add("C:/Users/Public/Public Projects/TheGreatBalloonRace/Source/TheGreatBalloonRace/VRBase");
-        PublicIncludePaths.Add("C:/Users/Public/Documents/Public Projects/TheGreatBalloonRace/Source/TheGreatBalloonRace/VRBase");
-
-        // Uncomment if you are using Slate UI
-        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-
-        // Uncomment if you are using online features
-        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+        PublicIncludePaths.Add(Path.GetFullPath(Path.Combine(ModuleDirectory, "VRBase")));
     }
 }

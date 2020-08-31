7d53d0db8630d0330f279b6019b14cffd3b44ff7
apaullo11 <58279316+apaullo11@users.noreply.github.com>
2020-08-31 15:18:41 -0400

    Updating to unreal v4.25 default build configuration
    Setting PCHUsage is no longer explicitly required as it is now default. bLegacyPublicIncludePaths is also set to false by default

--- Source/TheGreatBalloonRace/TheGreatBalloonRace.Build.cs
+++ Source/TheGreatBalloonRace/TheGreatBalloonRace.Build.cs
@@ -5,8 +5,6 @@ public class TheGreatBalloonRace : ModuleRules
 {
     public TheGreatBalloonRace(ReadOnlyTargetRules Target) : base(Target)
     {
-        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-
         PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Slate", "SlateCore", "ProceduralMeshComponent" });
         PrivateDependencyModuleNames.AddRange(new string[] { "HeadMountedDisplay", "SteamVR" });
 
